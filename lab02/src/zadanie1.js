72cffc6e4d63ec1161df6aa96697f311273c19bd
Ludwika Malinowska <malinowska.L@wp.pl>
2020-04-22 12:24:44 +0200

    zadanie 1.2

--- lab02/src/zadanie1.js
+++ lab02/src/zadanie1.js
@@ -1,6 +1,19 @@
 "use strict"
 
+//1.1
 const sum = (arr) => arr.reduce((x, y) => x + y, 0)
 console.log(sum([1, 2, 3, 4, 5, 6, 7]))
 
-const delet = (arr) => arr.reduce()
+//1.2
+const delet = (arr) => arr.reduce((prev, curr, index, arr) => {
+    // console.log(curr)
+    if (curr < 0){
+        arr.splice(index, 1)
+    }
+
+    return arr
+}, arr)
+
+const arr1 = [1, -2, 3, -4, 5, 6, 7]
+const arr2 = delet(arr1)
+console.log(arr2)
