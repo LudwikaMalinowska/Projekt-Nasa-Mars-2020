02176f40c2507b69be69485b922e6b9e4e3250ba
Ludwika Malinowska <malinowska.L@wp.pl>
2020-04-22 12:57:35 +0200

    zadanie 1.3

--- lab02/src/zadanie1.js
+++ lab02/src/zadanie1.js
@@ -21,3 +21,14 @@ const delet = (arr) => arr.reduce((prev, curr, index, arr) => {
 const arr1 = [1, -2, 3, -4, 5, 6, 7]
 const arr2 = delet(arr1)
 console.log(arr2)
+
+//zad 1.3
+const indeksy = (arr) => arr.reduce((prev, curr, index, arr) => {
+
+    console.log(index + ": " + curr)
+}, 0)
+
+console.log("\nZadanie 1.3:")
+const arr3 = [1, -2, 3, -4, 5, 6, 7]
+console.log(arr3)
+indeksy(arr3)
