b779879fa4ea7400e626461cea793c4344baa67e
Ludwika Malinowska <malinowska.L@wp.pl>
2020-04-22 11:43:56 +0200

    zadanie 1.1

---
+++ lab02/es5out/zadanie1.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const sum = arr => arr.reduce((x, y) => x + y, 0);
+
+print(sum([1, 2, 3, 4, 5, 6, 7]));
---
+++ lab02/src/zadanie1.js
@@ -0,0 +1,6 @@
+"use strict"
+
+const sum = (arr) => arr.reduce((x, y) => x + y, 0)
+console.log(sum([1, 2, 3, 4, 5, 6, 7]))
+
+const delet = (arr) => arr.reduce()
