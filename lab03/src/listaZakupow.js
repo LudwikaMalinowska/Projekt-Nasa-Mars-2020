ca8a5a5c37816be51d196481b5e49af5f9955c41
Ludwika Malinowska <malinowska.L@wp.pl>
2020-04-29 13:58:30 +0200

    zadanie 3.1

---
+++ lab03/src/listaZakupow.js
@@ -0,0 +1,93 @@
+﻿const listaZakupow = [
+    {
+        produkt: "chleb",
+        typ: "pieczywo",
+        ilosc: 2,
+        cena: 3.6,
+        jednostka: "sztuk"
+    },
+    {
+        produkt: "jabłka",
+        typ: "owoce",
+        ilosc: 6,
+        cena: 2.5,
+        jednostka: "kg"
+    },
+    {
+        produkt: "mleko",
+        typ: "nabiał",
+        ilosc: 3,
+        cena: 2.9,
+        jednostka: "litry"
+    },
+    {
+        produkt: "kawa",
+        typ: "napoje",
+        ilosc: 1,
+        cena: 24,
+        jednostka: "sztuk"
+    },
+    {
+        produkt: "kefir",
+        typ: "nabiał",
+        ilosc: 2,
+        cena: 2.4,
+        jednostka: "sztuk"
+    },
+    {
+        produkt: "woda",
+        typ: "napoje",
+        ilosc: 6,
+        cena: 1.9,
+        jednostka: "sztuk"
+    },
+    {
+        produkt: "marchewka",
+        typ: "warzywa",
+        ilosc: 2,
+        cena: 4,
+        jednostka: "kg"
+    },
+    {
+        produkt: "banan",
+        typ: "owoce",
+        ilosc: 1,
+        cena: 4.6,
+        jednostka: "kg"
+    },
+    {
+        produkt: "herbata",
+        typ: "napoje",
+        ilosc: 2,
+        cena: 8,
+        jednostka: "sztuk"
+    },
+    {
+        produkt: "ziemniaki",
+        typ: "warzywa",
+        ilosc: 5,
+        cena: 3.5,
+        jednostka: "kg"
+    },
+    {
+        produkt: "jogurt",
+        typ: "nabiał",
+        ilosc: 8,
+        cena: 1.4,
+        jednostka: "sztuk"
+    }
+]
+
+//3.1
+console.log("3.1:")
+listaZakupow.reduce((prev, curr, index, arr) => {
+    curr.cena = curr.cena * curr.ilosc
+    delete curr.ilosc;
+    if (index === 1) {
+        prev.cena = prev.cena * prev.ilosc
+        delete prev.ilosc;
+    }
+});
+console.log(listaZakupow)
+
+
