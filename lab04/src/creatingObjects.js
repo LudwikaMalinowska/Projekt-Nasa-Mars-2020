cbc41c83ab55ac2970411c9f206cdd93823b221f
Ludwika Malinowska <malinowska.L@wp.pl>
2020-05-06 11:45:11 +0200

    Zadanie 1.2

--- lab04/src/creatingObjects.js
+++ lab04/src/creatingObjects.js
@@ -45,7 +45,6 @@ function BookCreator(title, author) {
         console.log(b.author + " - " + b.title)
         // console.log("aaaaaa")
     };
-
 }
 
 const book3 = new BookCreator('Cień wiatru', 'Carlos Ruiz Zafon');
@@ -67,25 +66,33 @@ function testThis() {
     console.log(this);
 }
 
-testThis();
+testThis(); //obiekt globalny
 
 function testThis2() {
     "use strict"
     console.log(this);
 }
 
-testThis2();
+testThis2(); //undefined
 
 // 2.1. Czym jest this?
+// this odwołuje się do obiektu, w którym zostało wywołane
+// na przykład w book02 this.author odwołuje się do właściwości author obiektu book02
+// ale żeby odwołać cię do tej właściwości wewnątrz obiektu, trzeba użyć this
+
 // 2.2. Do czego odwołuje się this w obu przypadkach
+// w pierwszym przypadku this odwołuje się do obiektu globalnego, natomiast
+// wynik drugiej funkcji to undefined
+
 
 const person = {
     name: 'Oscar Wilde',
     print() {
-        console.log(this.name);
+        console.log("a: " + this.name); // oscar wilde
+        // console.log(this); // tu byłoby: { name: 'Oscar Wilde', print: [Function: print] }
 
         function a() {
-            console.log(this);
+            console.log(this); // obiekt globalny
         }
         a();
     }
@@ -93,6 +100,11 @@ const person = {
 person.print();
 
 // 2.3. Jakie wartości przyjmuje this w powyższych przypadkach i dlaczego?
+// this.name przyjmuje wartość 'Oscar Wilde', bo odwołuje się do własności name obiektu person
+// this z funkcji a odwołuje się do obiektu globalnego, ponieważ nie jest bezpośrednio w obiekcie,
+// tylko w funkcji a wewnątrz funkcji print obiektu
+// jakby this było poza funkcją a, odnosiłoby się do obiektu person
+
 // 2.4. Zmodyfikuj powyższy kod w ten sposób, aby funkcja a wyświetlała w konsoli 'a: Oscar Wilde'. Nie używaj arrow function.
 
 const printName = function() {
@@ -100,14 +112,21 @@ const printName = function() {
 }
 
 const person1 = {
-    name: 'Aaron Towels'
+    name: 'Aaron Towels',
+    print: printName
 }
 
 const person2 = {
-    name: 'Tom Clancy'
+    name: 'Tom Clancy',
+    print: printName
 }
 
+console.log("\nperson 1 i person 2:")
+person1.print();
+person2.print();
+
 // 2.5. Za pomocą funkcji printName wypisz 'name' obu autorów. Nie zmieniaj implementacji funkcji printName!
+
 
 const person3 = {
     name: 'Arthur Conan Doyle',
@@ -120,7 +139,11 @@ const person3 = {
 }
 person3.print();
 
-// 2.6. Co wydrukuje w konsoli powyższy kod? Jaki scope ma arrow function? 
+// 2.6. Co wydrukuje w konsoli powyższy kod? Jaki scope ma arrow function?
+// { name: 'Arthur Conan Doyle', print: [Function: print] }
+// funkcja strzałkowa nie ma własnego this i w tym wypadku this odnosi się do obiektu person3,
+// (w przypadku zwykłej funkcji this odnosiłoby się do obiektu globalnego)
+
 
 // ========================================
 // ZADANIE 3
