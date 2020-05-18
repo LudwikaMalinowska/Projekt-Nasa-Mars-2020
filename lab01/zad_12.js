5d959ca64014729ae8b65b0b7c147d2a75a69fef
Ludwika Malinowska <malinowska.L@wp.pl>
2020-05-18 23:26:55 +0200

    Lab 1 - zadanie 12 - poprawione, wczesniejszy commit 12go zadania to byla pomylka, pomylilam numery zadan

--- lab01/zad_12.js
+++ lab01/zad_12.js
@@ -1,13 +1,24 @@
 "use strict"
 
-const czyString = function (a) {
-    const result = (typeof a === 'string') ? true : false;
-    return result;
+const ntyNajwiekszy = function (tab, n) {
+    const najwieksze = []
+
+    for (let i = 1; i <= n; i++){
+        const max = Math.max(...tab)
+        najwieksze.push(max);
+        let pos = tab.indexOf(max);
+        while(pos !== -1){
+            tab.splice(pos, 1);
+            pos = tab.indexOf(max);
+        }
+    }
+    // console.log(najwieksze);
+    // console.log(tab)
+    return najwieksze[n-1];
 }
 
-const a = "abc";
-const b = 1;
-const c = czyString(a);
-const d = czyString(b);
-console.log(c);
-console.log(d);
+
+
+const a = [2,3,1,5,6,7,7,8,8,8,8,9,9,9,2,1,4];
+const nty = ntyNajwiekszy(a, 3)
+console.log(nty)
