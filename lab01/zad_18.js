f05273c80a9a4b227b2267b5a28a2eeba0d519a2
Ludwika Malinowska <malinowska.L@wp.pl>
2020-05-19 19:16:34 +0200

    Lab 1 - zadanie 18 - poprawione, znowu pomylka

--- lab01/zad_18.js
+++ lab01/zad_18.js
@@ -1,18 +1,11 @@
 "use strict"
 
-const najdluzszyWyraz = (function (str) {
+const f = function () {
 
-    let str2 = str.split(" ")
-    // console.log(str2);
-    let max = str2[0];
-    for (let i = 1; i < str2.length; i++){
-        if (str2[i].length > max.length){
-            max = str2[i];
-        }
+    const num = 5
+    return function (number) {
+        return num + number;
     }
+}
 
-    return max;
-})("Ala ma kota");
-
-
-console.log(najdluzszyWyraz);
+console.log(f()(2))
