c97c0d567fd7bc191990e035e8122a4c0c2c5405
Ludwika Malinowska <malinowska.L@wp.pl>
2020-05-21 19:36:54 +0200

    Lab 2 - zadanie 1.3 - dodano sredniki

--- lab02/src/zadanie1.js
+++ lab02/src/zadanie1.js
@@ -25,19 +25,19 @@ console.log(arr2)
 //zad 1.3
 const indeksy = (arr) => arr.reduce((prev, curr, index, arr) => {
 
-    console.log(index + ": " + curr)
-}, 0)
+    console.log(index + ": " + curr);
+}, 0);
 
-console.log("\nZadanie 1.3:")
-const arr3 = [1, -2, 3, -4, 5, 6, 7]
-console.log(arr3)
-indeksy(arr3)
+console.log("\nZadanie 1.3:");
+const arr3 = [1, -2, 3, -4, 5, 6, 7];
+console.log(arr3);
+indeksy(arr3);
 
 //zadanie 1.4
 const arr4 = ['js', 'react', 'js', 'angular', 'angular', 'js']
 const powtozenia = (arr) => arr.reduce((prev, curr, index, arr) => {
 
-    
+
     // if (isNaN(dict[`${curr}`])) {
     //     dict[`${curr}`] = 1
     // } else {
