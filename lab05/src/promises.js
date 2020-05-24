5232eb3cf3c87a34f526013f7ee1efd5fd5d4558
Ludwika Malinowska <malinowska.L@wp.pl>
2020-05-24 20:54:24 +0200

    Lab 5 - zadanie 3.4

--- lab05/src/promises.js
+++ lab05/src/promises.js
@@ -25,14 +25,43 @@
 // zakończył się porażką i zwracał string 'Porażka'.
 // Skorzystaj z then() aby obsłużyć błąd.
 
-const prom = new Promise( (resolve, reject) => {
-    const waitTo = new Date().getTime() + 5000;
-    while(waitTo > new Date().getTime()) {}
+// const prom = new Promise( (resolve, reject) => {
+//     const waitTo = new Date().getTime() + 1000;
+//     while(waitTo > new Date().getTime()) {}
+//
+//
+//     // return resolve("Udało się!");
+//     return reject("Porażka");
+// })
+//
+// function resolved(message) {
+//     console.log(message);
+// }
+//
+// function rejected(message) {
+//     console.log(message);
+// }
+
+// prom.then(resolved).catch(rejected);
 
 
-    // return resolve("Udało się!");
-    return reject("Porażka")
-})
+// 3.3 Zamiast then(), zmodyfikuj powyższy kod używając catch()
+
+// 3.4 Napisz funkcję multiplyAsync(x,y), która zwraca obiekt klasy Promise, kończący 
+// się porażką, gdy któryś za argumentów jest niepoprawny (nie jest liczbą).
+// W przeciwnym przypadku zwraca iloczyn dwóch liczb. Napisz callback, który wypisuje wynik
+// w konsoli.
+
+function multiplyAsync(x,y) {
+    return new Promise( (resolve, reject) => {
+        if (typeof x !== "number" || typeof y !== "number"){
+            return reject("Porażka.")
+        } else {
+            return resolve(x*y)
+        }
+        }
+    )
+}
 
 function resolved(message) {
     console.log(message);
@@ -42,13 +71,7 @@ function rejected(message) {
     console.log(message);
 }
 
-prom.then(resolved).catch(rejected);
-
-
-// 3.3 Zamiast then(), zmodyfikuj powyższy kod używając catch()
-
-// 3.4 Napisz funkcję multiplyAsync(x,y), która zwraca obiekt klasy Promise, kończący się porażką, gdy któryś za argumentów jest niepoprawny (nie jest liczbą).
-// W przeciwnym przypadku zwraca iloczyn dwóch liczb. Napisz callback, który wypisuje wynik w konsoli.
+multiplyAsync(5, 2).then(resolved).catch(rejected);
 
 // 3.5 Dołącz axios do projektu. Wykonaj funkcję get dla następującego url: https://jsonplaceholder.typicode.com/posts. 
 // Jako pierwszy callback - sprawdź czy response jest poprawny (status równy 200). Jeśli tak, to zwróć response, w przeciwnym wypadku wypisz błąd w konsoli.
