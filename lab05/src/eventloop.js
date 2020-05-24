f6491f3852dd833438a303cdeb8b8985a8b08670
Ludwika Malinowska <malinowska.L@wp.pl>
2020-05-24 14:41:21 +0200

    Lab 5 - zadanie 2.1

---
+++ lab05/src/eventloop.js
@@ -0,0 +1,29 @@
+'use strict';
+// Zadanie 2.1. Dopisz do pomiędzy deklaracją funkcji helloWorld, a poleceniem
+// console.log instrukcję wywołania helloWorld() tak, aby na ekranie pojawiło się
+// jako pierwsze 'No, hello universe!'.
+// Nie możesz dopisać nic za console.log()
+
+
+function helloWorld() {
+    console.log('Hello world!');
+}
+
+setTimeout(() => {
+    helloWorld()
+}, 1000);
+
+
+
+console.log('No, hello universe!');
+
+// Zadanie 2.2. Napisz funkcję, która wypisuje w konsoli 'Start!' i po dwóch sekundach wypisuje 'Koniec'.
+
+// Zadanie 2.3. Napisz funkcję, która wypisuje 'Welcome' co sekundę w nieskończoność.
+
+// Zadanie 2.4. Napisz funkcję, która wypisuje 'Welcome' co sekundę, ale tylko przez 5 sekund. Podpowiedź: (użyj clearInterval)
+
+// Zadanie 2.5. Napisz funkcję, która przyjmuje trzy argumenty: funkcję i dwie liczby. Funkcja będzie wywołała podaną w argumencie funkcję co x milisekund i automatycznie zatrzyma się po upływie y milisekund.
+
+  
+  
