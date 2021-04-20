026b591c88dfbb87d6ba7913200ac21a63dcf8ee
Ludwika Malinowska <58569359+LudwikaMalinowska@users.noreply.github.com>
2021-04-20 11:00:30 +0200

    Delete lab01 directory

--- lab01/zad_02.js
+++
@@ -1,6 +0,0 @@
-"use strict"
-
-const toArray = (a, b) => [a, b]
-
-const arr = toArray(1, 2)
-console.log(arr)
--- lab01/zad_16.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-const hello = function (str) {
-    const result = (str === undefined) ? "Hello world" : "Hello " + str;
-    console.log(result)
-}
-
-hello()
-hello("aaa")
